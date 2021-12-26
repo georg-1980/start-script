ebfcc0caa4f88e787495e1af5ea56a757fb0e364
georg-1980 <georg.dahmen.test@web.de>
2021-12-26 17:23:10 +0000

    heausnehmen von nicht benotigter einlesefuntion

--- start-script.sh
+++ start-script.sh
@@ -2,4 +2,4 @@ echo "Hello world Georg 01" > test01.txt
 
 	
 
-while read line; do datei=${line/;*/}; wert=${line/$datei;/}; echo $wert > "$datei"; done < test.txt
+#while read line; do datei=${line/;*/}; wert=${line/$datei;/}; echo $wert > "$datei"; done < test.txt
